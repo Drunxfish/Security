800d8926252ff469085c5435be5efb7db6945692
Alexandre charbadze <133922150+Drunxfish@users.noreply.github.com>
2025-05-12 12:36:31 +0200

    Caesar Cipher
    Understanding Caesar Cipher.

---
+++ MD5-Hash/caesarCipher.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = readline("Enter a string: ");
+$shift = readline("Enter a shift value: ");
+
+
+function caesarCipher($str, $shift)
+{
+    $cipher = "";
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (ctype_upper($str[$i])) {
+            $cipher .= chr((ord($str[$i]) + $shift - 65) % 26 + 65);
+        } else {
+            $cipher .= chr((ord($str[$i]) + $shift - 97) % 26 + 97);
+        }
+    }
+
+    print ("------------------------\nText: $str \nShift: $shift \nCipher: $cipher");
+    return $cipher;
+}
+
+caesarCipher($str, $shift);
